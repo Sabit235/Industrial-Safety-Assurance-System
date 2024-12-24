621bc4bd0787128bdc8ed80eab3b99bd4683fd4e
Sabit Hasan <sabithasan2352002@gmail.com>
2024-12-25 03:57:43 +0600

    Temperature and humidity sensor added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//! Pin Definitions modify pin here
+int gasSensorInputPin = 13;
+int gasSensorOutputPin = 23;
+int gasSensorThreshold = 2000;
+
+//int tempSensorInputPin = 32;
+int tempSensorOutputPin = 22;
+
+#define DHTPIN 32    // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
+//!---------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(gasSensorInputPin, INPUT);
+  pinMode(gasSensorOutputPin, OUTPUT);
+
+  // pinMode(tempSensorInputPin, INPUT);
+  // pinMode(tempSensorOutputPin, OUTPUT);
+
+  dht.begin();
+
+  Serial.println("Initialization Complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int gasSensorValue = analogRead(gasSensorInputPin);
+  Serial.println(gasSensorValue);
+
+  if (gasSensorValue > gasSensorThreshold)
+  {
+    digitalWrite(gasSensorOutputPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(gasSensorOutputPin, LOW);
+  }
+  
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+
+  delay(250);
 }
